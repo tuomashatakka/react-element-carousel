8cf6c7054ca9c28139bad2152915dfff8b427303
Tuomas Hatakka <tuomas.hatakka@teliacompany.com>
2022-07-11 10:37:14 +0300

    Add a couple of additional carousels for demonstration

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import Carousel from './Carousel'
 
-export const App = () =>
+export const App = () => <>
   <Carousel>
     <article>
       <h2>Lorem ipsum</h2>
@@ -77,3 +77,25 @@ export const App = () =>
       <p>dolor sit amet etc...</p>
     </article>
   </Carousel>
+  <div style={{ height: '10vh'}}></div>
+  <Carousel>
+    <article>
+      <h2>Short</h2>
+    </article>
+    <article>
+      <h2>Carousel</h2>
+      <p>...</p>
+    </article>
+    <article>
+      <h2>one</h2>
+      <p>...</p>
+    </article>
+  </Carousel>
+  <div style={{ height: '10vh'}}></div>
+  <Carousel>
+    <article>
+      <h2>Even shorter carousel</h2>
+    </article>
+    <span>asd</span>
+  </Carousel>
+</>
